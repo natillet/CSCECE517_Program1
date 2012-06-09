faea0a77c8a9be30b2c52e0d2cc758f0f7124d72
unknown <endahner@vclh3-7-2946.hpc.ncsu.edu>
2012-06-09 14:39:48 -0400

    Initialized Rails with RSpec

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CSCECE517Program1::Application.config.secret_token = '1d1e3c49a5497f4b1e999fd29b8e52f7ebda906f5d839da59f3f68da94cd70f8c5b097f0186ad361bbc3c1115f581e09f8b3fc998da2ca1324dcd7533fb0773e'
