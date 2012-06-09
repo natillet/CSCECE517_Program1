6468396cba78a7eb2545881107c696f5620a448c
Elene Trull <endahner.edu>
2012-06-09 17:34:51 -0400

    Comments seem to basically work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 CSCECE517Program1::Application.routes.draw do
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
