afebb40ba45aab5a68b16f1b6f1aec02d76f3a0f
unknown <endahner@vclh3-8-2946.hpc.ncsu.edu>
2012-06-17 13:08:47 -0400

    Search spec cases added

--- spec/requests/search_spec.rb
+++ spec/requests/search_spec.rb
@@ -13,14 +13,62 @@ describe "Search" do
       response.status.should be(200)
     end
 
-    #it "logs in as admin" do
-    #  visit login_path
-    #  fill_in "Name", :with => (@user.name)
-    #  fill_in "Password", :with => (@password)
-    #  click_button "Login"
-    #  page.should have_content "Welcome to being logged in, #{@user.name}"
-    #  current_path.should == admin_path
-    #end
+    it "finds search on main page" do
+      visit home_path
+      page.should have_content "Search"
+    end
+
+    it "submits a blank search" do
+      visit home_path
+      fill_in "search", :with => ""
+      click_button "Search"
+      current_path.should == home_path
+      page.should have_content "Search query cannot be empty"
+    end
+
+    it "submits a user search" do
+      @user = User.create :name => "elene", :password_digest => "elene", :is_admin => false
+      visit home_path
+      fill_in "search", :with => "en"
+      page.select("by Username", :from => "searchby_searchbyid")
+      click_button "Search"
+      current_path.should == '/search/index'
+      page.should have_content(@user.name)
+    end
+
+    it "submits a post search" do
+      @user = User.create :name => "elene", :password_digest => "elene", :is_admin => false
+      @post = Post.new
+      @post.post = "Inspector Gadget here!"
+      @post.user_id = @user.id
+      @post.save
+      visit home_path
+      fill_in "search", :with => "Gadget"
+      page.select("by Post", :from => "searchby_searchbyid")
+      click_button "Search"
+      current_path.should == '/search/index'
+      page.should have_content(@post.post)
+    end
+
+    it "submits a comment search" do
+      @user = User.create :name => "elene", :password_digest => "elene", :is_admin => false
+      @post = Post.new
+      @post.post = "Inspector Gadget here!"
+      @post.user_id = @user.id
+      @post.save
+      @comment = Comment.new
+      @comment.comment = "Go go gadget arms!"
+      @comment.user_id = @user.id
+      @comment.post_id = @post.id
+      @comment.save
+      visit home_path
+      fill_in "search", :with => "gadget"
+      page.select("by Comment", :from => "searchby_searchbyid")
+      click_button "Search"
+      current_path.should == '/search/index'
+      page.should have_content(@comment.comment)
+    end
+
   end
 
 end
