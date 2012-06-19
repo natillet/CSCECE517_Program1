ddb00b30c55e8a3eb1794539e77bf94b03788af4
Neelam Chitnis <ncchitni@ncsu.edu>
2012-06-19 14:10:48 -0400

    test for admin and statistics controller

--- spec/controllers/statistics_controller_spec.rb
+++ spec/controllers/statistics_controller_spec.rb
@@ -1,10 +1,24 @@
 require 'spec_helper'
 
 describe StatisticsController do
+  def valid_attributes
+  {
+      :name => "admin",
+      :password_digest => "admin_pass",
+      :is_admin => true
+  }
+  end
+  def valid_session
+    {
+        :user_id => 1    #assumes that there is a first user who is an admin
+    }
+  end
 
   describe "GET 'index'" do
     it "returns http success" do
-      get 'index'
+      user = User.create! valid_attributes
+      get :index, {}, valid_session
+      #get 'index'
       response.should be_success
     end
   end
